3cf8bb87ea27bd4bf33602487b90ccd314091881
demostudentgwl <72288425+demostudentgwl@users.noreply.github.com>
2021-12-11 19:55:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react'
+
+import BottomTabNavigator from './components/BottomTabNavigator'
+//import TransactionScreen from "./screens/transaction";
+//import SearchScreen from "./screens/search";
+
+
+
+export default class App extends Component{
+    render(){
+return(
+  <BottomTabNavigator/>
+)}
+}
